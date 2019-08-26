4eebd222e169b1fa6fc4262f182f073e6ed36c43
Lauren Garcia <lauren.garcia@gmail.com>
2019-08-26 16:06:46 -0700

    put Kovan address in 2_deploy_contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,14 +6,15 @@ module.exports = function(deployer) {
    * Second arg is the 0x Exchange contract location that the Whitelist will
    * forward the order to after it verifies Whitelist status of maker + taker:
    */
-
-  // TODO: process.env Exchange contract argument
-  deployer.deploy(Whitelist, "0x080bf510fcbf18b91105470639e9561022937712");
+  deployer.deploy(
+    Whitelist,
+    "0x30589010550762d2f0d06f650D8e8B6Ade6DBf4b" // Ox Exchange on Kovan
+  );
 };
 
 // 0x Exchange contract
-// Kovan:
+// Kovan (aka 42):
 // 0x30589010550762d2f0d06f650D8e8B6Ade6DBf4b
 
-// Prod:
+// Mainnet:
 // 0x080bf510fcbf18b91105470639e9561022937712
