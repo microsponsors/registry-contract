52e0a13b62cb55baaea567414467b8e38d76e489
Lauren Garcia <lauren.garcia@gmail.com>
2019-09-10 18:55:34 -1000

    isContentIdRegisteredToCaller()

--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -262,6 +262,8 @@ contract Whitelist is
 
 
     /// @dev Valid whitelisted address can query its own contentIds.
+    ///      In practice, this is called from the Microsponsors dapp so a
+    ///      user can view their own content ids.
     function getContentIdsByAddress()
         external
         view
@@ -281,6 +283,34 @@ contract Whitelist is
         }
 
         return r;
+
+    }
+
+    /// @dev Valid whitelisted address can validate registration of its own single contentId.
+    ///      In practice, this will be used by Microsponsors' ERC-721 for
+    ///      validating that an address is authorized to mint() a time slot
+    ///      for a given content id.
+    function isContentIdRegisteredToCaller(string memory contentId)
+        external
+        view
+        returns(string memory)
+    {
+
+        // Check tx.origin vs msg.sender since this will be invoked by
+        // Microsponsors' ERC-721 contract
+        require(
+            isWhitelisted[tx.origin],
+            'INVALID_SENDER'
+        );
+
+        address registrantAddress = contentIdToAddress[contentId];
+
+        require(
+            registrantAddress == tx.origin,
+            'INVALID_SENDER'
+        );
+
+        return true;
 
     }
 
