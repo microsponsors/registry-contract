cfa1128166879c047b5657d65a7fa4660ec18fae
Lauren Garcia <lauren.garcia@gmail.com>
2019-12-29 13:34:13 -0800

    Update Ownable: rm @0x dep, set 2nd Owner

---
+++ contracts/IOwnable.sol
@@ -0,0 +1,13 @@
+// Adaped from "@0x/contracts-utils/contracts/src/interfaces/IOwnable.sol";
+
+pragma solidity ^0.5.5;
+
+
+contract IOwnable {
+
+    function transferOwnership1(address newOwner)
+        public;
+
+    function transferOwnership2(address newOwner)
+        public;
+}
--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -19,7 +19,7 @@
 pragma solidity ^0.5.5;
 pragma experimental ABIEncoderV2;
 
-import "@0x/contracts-utils/contracts/src/Ownable.sol";
+import "./Ownable.sol";
 
 
 contract Registry is
